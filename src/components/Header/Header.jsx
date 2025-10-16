dd33163726f3be3641ddd980d803838a51895fd9
Valeria Borgatti <valeriamborgatti3@gmail.com>
2025-10-15 22:40:34 -0300

    Corrijo HEADER

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ export const Header = () => {
   return (
     <header className="header">
       <div className="logo">
-        <img src="public/images/Round Photo_Oct202023_090136.png" alt="Logo" className="logo-img" /><h5>Collect Sun</h5> 
+        <img src="/images/Round_Photo_Oct202023_090136.png" alt="Logo" className="logo-img" /><h5>Collect Sun</h5> 
       </div>
 
       <Nav />
