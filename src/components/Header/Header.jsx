74588da3ae3025423106f58241a5ae9e7b15933e
Valeria Borgatti <valeriamborgatti3@gmail.com>
2025-11-07 18:39:21 -0300

    Actualizo proyecto con últimos cambios

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,39 @@
 import "./Header.css";
 import { Nav } from "../Nav/Nav";
 import { Link } from "react-router-dom";
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
 
 export const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <header className="header">
-     <Link to={"/"}>
-      <div className="logo">
-        <img src="/images/Round_Photo_Oct202023_090136.png" alt="Logo" className="logo-img" /><h5>Collectsun</h5> 
+      {/* 🔹 LOGO */}
+      <Link to={"/"}>
+        <div className="logo">
+          <img
+            src="/images/Round_Photo_Oct202023_090136.png"
+            alt="Logo"
+            className="logo-img"
+          />
+          <h5>Collectsun</h5>
+        </div>
+      </Link>
+
+      {/* 🔹 BOTÓN HAMBURGUESA */}
+      <button
+        className="menu-toggle"
+        onClick={() => setMenuOpen(!menuOpen)}
+        aria-label="Abrir menú"
+      >
+        {menuOpen ? <X size={22} /> : <Menu size={26} />}
+      </button>
+
+      {/* 🔹 CONTENEDOR DEL MENÚ */}
+      <div className={`nav-container ${menuOpen ? "open" : ""}`}>
+        <Nav />
       </div>
-    </Link>
-      <Nav />
     </header>
   );
 };
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,28 +1,25 @@
 import { Link } from "react-router-dom";
-import './Nav.css';
+import "./Nav.css";
 import { useCartContext } from "../../context/CartContext/useCartContext";
 
+export const Nav = ({ isOpen }) => {
+  const { getTotalItems } = useCartContext();
 
-export const Nav = () => {
-  const {getTotalItems}= useCartContext()
-
-    return (
-    
-     <nav>
+  return (
+    <nav className={`nav ${isOpen ? "open" : ""}`}>
       <ul>
         <li><Link to={"/"}>Inicio</Link></li>
-        <li> <Link to={"/category/accesorios"}>Accesorios</Link></li>
+        <li><Link to={"/category/accesorios"}>Accesorios</Link></li>
         <li><Link to={"/category/arte"}>Arte</Link></li>
-        
-        <li><Link to={"/carrito"}>Carrito</Link>
-        {getTotalItems()>0 && (
-           <span className="in-cart">{getTotalItems()}</span>
-             
-        )}
-        
+        <li>
+          <Link to={"/carrito"}>
+            Carrito
+            {getTotalItems() > 0 && (
+              <span className="in-cart">{getTotalItems()}</span>
+            )}
+          </Link>
         </li>
-        
       </ul>
     </nav>
-    );
+  );
 };
